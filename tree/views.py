a2f7a4f6fdbfad8caf3c1d794454c41033ee72aa
Владимир Гуровиц <gurovic@gmail.com>
2024-03-29 23:27:51 +0300

    bulk load from google doc

--- tree/views.py
+++ tree/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from .models import Category, Recipe
+from . import spreadsheet
 
-# Create your views here.
+def load_data(request):
+    return render(request, 'load_data.html')
+@require_http_methods(["GET"])
+def bulk_load(request):
+    # column 3 -> recipe.source
+    # column 4 -> recipe.subsource
+    # column 6 -> recipe.title
+
+    # column 7 -> recipe.category (new)
+    # column 8 -> recipe.category.thai_title
+    # column 9 -> recipe.category.thai_transcription_name
+    # column 11 -> recipe.category.parent
+    for line in spreadsheet.get_data(request.GET['start_line'], request.GET['finish_line']):
+        category = Category(title=line[7],
+                            thai_title=line[8],
+                            thai_transcript_name=line[9],
+                            parent_id=int(line[11]))
+        category.save()
+
+        recipe = Recipe(title=line[6],
+                        source_id=int(line[3]),
+                        subsource=line[4],
+                        category=category)
+        recipe.save()
+
+    return redirect('/')
