146cc26ebc990a5ac75f2c2e35bde5d5b3fe69ef
Владимир Гуровиц <gurovic@gmail.com>
2024-11-12 15:08:59 +0300

    PEP

--- tree/views.py
+++ tree/views.py
@@ -45,7 +45,9 @@ def get_tree_data(request, dish_type):
     elif dish_type == "F":
         objects = Category.objects.filter(Q(number_of_fish_recipes__gt=0) | Q(number_of_veg_recipes__gt=0))
     else:
-        objects = Category.objects.filter(Q(number_of_seafood_recipes__gt=0) | Q(number_of_fish_recipes__gt=0) | Q(number_of_veg_recipes__gt=0))
+        objects = Category.objects.filter(Q(number_of_seafood_recipes__gt=0) |
+                                          Q(number_of_fish_recipes__gt=0) |
+                                          Q(number_of_veg_recipes__gt=0))
 
     for obj in objects:
         if dish_type == "M":
@@ -59,7 +61,7 @@ def get_tree_data(request, dish_type):
 
         tree_data.append({
             'id': obj.id,
-            'parent': obj.parent_id if obj.parent else '#',  # '#' указывает на корневой уровень
+            'parent': obj.parent_id if obj.parent else '#',  # '#' points to root level
             'text': f"{obj.title} ({count})",
             'a_attr': {'href': f'/dish/{obj.id}/{dish_type}'}
         })
@@ -85,6 +87,7 @@ def bulk_load(request):
 
     # column 14 -> recipe.ingredients
     for line in spreadsheet.get_data(request.GET['start_line'], request.GET['finish_line']):
+        # line[11](L) != "" or line[7](H) == "-"
         if line[7] != '-':
             category = Category(title=line[7],
                                 thai_title=line[8],
@@ -94,11 +97,13 @@ def bulk_load(request):
         else:
             category = Category.objects.get(pk=int(line[11]))
 
+        # line[6](G) != "" and line[3](D) != "" and line[14](O) != "" and line[17](R) != ""
         recipe = Recipe(title=line[6],
                         source_id=int(line[3]),
                         subsource=line[4],
                         link=line[5],
                         category=category,
+                        # TODO refactor for new model
                         ingredients=line[14],
                         vegetarian=line[17])
         recipe.save()
@@ -108,8 +113,8 @@ def bulk_load(request):
 
 def show_tree(request, dish_type="M"):
     return render(request, "NiceAdmin/index.html", {'categories': Category.objects.all(),
-                                         'dish_type': dish_type,
-                                         'news': News.objects.all()})
+                                                    'dish_type': dish_type,
+                                                    'news': News.objects.all()})
 
 
 def show_dish(request, id, dish_type):
@@ -148,7 +153,7 @@ def recipe_number(cat):
     cat.number_of_fish_recipes = result_f
     cat.number_of_seafood_recipes = result_s
     cat.save()
-    return (result, result_v, result_f, result_s)
+    return result, result_v, result_f, result_s
 
 
 def refresh_recipe_numbers(request):
@@ -179,14 +184,14 @@ def user_login(request):
 def select_cooked(request, recipe_id, cooked):
     recipe = Recipe.objects.get(id=recipe_id)
     recipe.users.remove(request.user)
-    recipe.users.add(request.user, through_defaults={"cooked":cooked})
+    recipe.users.add(request.user, through_defaults={"cooked": cooked})
     recipe.save()
     return HttpResponse()
 
 
 def user_recipes(request, dish_type):
-    user_recipes = request.user.userreciperelation_set.all()
-    return render(request, 'NiceAdmin/user_recipes.html', {'dish_type': dish_type, 'user_recipes': user_recipes})
+    recipes = request.user.userreciperelation_set.all()
+    return render(request, 'NiceAdmin/user_recipes.html', {'dish_type': dish_type, 'user_recipes': recipes})
 
 
 def ingredient_dishes(request, ingredient_id):
@@ -202,9 +207,9 @@ def ingredient_dishes(request, ingredient_id):
 def ingredient_list(request):
     ingredient_types = IngredientType.objects.all()
     result = dict()
-    for object in ingredient_types:
-        ingredients = Ingredient.objects.filter(ingredient_type=object).order_by('name')
-        result[object] = ingredients
+    for obj in ingredient_types:
+        ingredients = Ingredient.objects.filter(ingredient_type=obj).order_by('name')
+        result[obj] = ingredients
     return render(request, 'NiceAdmin/ingredients_list.html', {'ingredients': result})
 
 
